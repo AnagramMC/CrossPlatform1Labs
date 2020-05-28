0be34b209ec5b0afd8fd8c15a1b45d8d19bc8c31
hisham-css <hisham.ata@hotmail.com>
2020-05-28 11:26:59 -0400

    Added unity project

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    GameManager instance;
+    Rigidbody2D myRigidBody;
+
+    public float bounceForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = FindObjectOfType<GameManager>();
+        myRigidBody = GetComponent<Rigidbody2D>();
+
+        if (bounceForce <= 0)
+        {
+            bounceForce = 20.0f;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "EnemySquish")
+        {
+            collision.gameObject.GetComponentInParent<Enemy_Walker>().IsSquished();
+
+            myRigidBody.velocity = Vector2.zero;
+            myRigidBody.AddForce(Vector2.up * bounceForce);
+        }
+    }
+}
